887d869a355ec654ae6de8db298821707b8fee81
AnthonyMde <anthony.mamode@gmail.com>
2018-06-04 12:05:27 +0200

    Add toolbar.xml file. Add the toolbar and the toolbar menu layout.

--- app/src/main/java/com/mamode/anthony/mynews/MainActivity.java
+++ app/src/main/java/com/mamode/anthony/mynews/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mamode.anthony.mynews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Parameters", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Searching...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void configureToolbar() {
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
