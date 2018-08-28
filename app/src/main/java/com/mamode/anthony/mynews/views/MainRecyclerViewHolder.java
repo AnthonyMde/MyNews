ab275c0f0b8a7272c06d04b1f94aa6ffab685897
AnthonyMde <anthony.mamode@gmail.com>
2018-08-28 23:01:36 +0200

    TopStories imageView retrieve from API data with Glide

--- app/src/main/java/com/mamode/anthony/mynews/views/MainRecyclerViewHolder.java
+++ app/src/main/java/com/mamode/anthony/mynews/views/MainRecyclerViewHolder.java
@@ -1,19 +1,27 @@
 package com.mamode.anthony.mynews.views;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.mamode.anthony.mynews.R;
+import com.mamode.anthony.mynews.fragments.TopStoriesFragment;
 import com.mamode.anthony.mynews.models.Article;
+import com.mamode.anthony.mynews.models.Multimedium;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainRecyclerViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.recycler_item_snippet) TextView snippet;
-    @BindView(R.id.recycler_item_ariane) TextView ariane;
+    @BindView(R.id.recycler_item_ariane) TextView ariadneThread;
     @BindView(R.id.recycler_item_date) TextView date;
+    @BindView(R.id.recycler_item_image) ImageView image;
 
 
     public MainRecyclerViewHolder(View itemView) {
@@ -29,8 +37,14 @@ public class MainRecyclerViewHolder extends RecyclerView.ViewHolder {
         if (!article.getSubsection().equals("")){
             ariane += " > " + article.getSubsection();
         }
-        this.ariane.setText(ariane);
+        this.ariadneThread.setText(ariane);
 
         this.date.setText(article.getUpdatedDate());
+
+        List<Multimedium> multimedia = article.getMultimedia();
+        if (multimedia.size() != 0) {
+            String url = multimedia.get(0).getUrl();
+            Glide.with(image.getContext()).load(url).into(image);
+        }
     }
 }
