889207fbc4f2b298716e173e42ded424d2ccde62
AnthonyMde <anthony.mamode@gmail.com>
2018-09-28 10:45:11 +0200

    Refactoring and commenting code in SearchActivity.

--- app/src/main/java/com/mamode/anthony/mynews/controllers/SearchActivity.java
+++ app/src/main/java/com/mamode/anthony/mynews/controllers/SearchActivity.java
@@ -1,7 +1,6 @@
 package com.mamode.anthony.mynews.controllers;
 
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -9,14 +8,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mamode.anthony.mynews.R;
@@ -63,7 +59,7 @@ public class SearchActivity extends AppCompatActivity{
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                doWeSetButtonClickable();
+                enableSearchIfConditionMet();
             }
 
             @Override
@@ -81,18 +77,16 @@ public class SearchActivity extends AppCompatActivity{
             ab.setDisplayHomeAsUpEnabled(true);
     }
 
-    private void doWeSetButtonClickable(){
-        if(mInput.getText().length()>=3){
-            if(checkboxCounter>=1)mSearchButton.setEnabled(true);
-            else{mSearchButton.setEnabled(false);}
-        }else{mSearchButton.setEnabled(false);}
+    // Search button is enabled or disabled according to the required conditions.
+    // At least 3 letters in the input and 1 checkbox checked
+    private void enableSearchIfConditionMet(){
+        mSearchButton.setEnabled(mInput.getText().length()>=3 && checkboxCounter>=1);
     }
 
-    // Method call from the layout file
+    // Method call from the fragment_search_and_notif layout file
     public void onCheckboxClicked(View view){
-        if(((CheckBox)view).isChecked()){checkboxCounter++;}
-        else{checkboxCounter--;}
-        doWeSetButtonClickable();
+        checkboxCounter = ((CheckBox)view).isChecked() ? checkboxCounter+1 : checkboxCounter-1;
+        enableSearchIfConditionMet();
     }
 
     private void setActualDate(){
@@ -112,35 +106,39 @@ public class SearchActivity extends AppCompatActivity{
                 datePickerDialog = new DatePickerDialog(SearchActivity.this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        if (datePickerEditText == beginDate){
-                            beginDateValue = Utils.addZeroToDate(dayOfMonth)+"/"+ Utils.addZeroToDate(month+1)+"/"+year;
-                            datePickerEditText.setText(beginDateValue);
-                        }else {
-                            endDateValue = Utils.addZeroToDate(dayOfMonth)+"/"+ Utils.addZeroToDate(month+1)+"/"+year;
-                            datePickerEditText.setText(endDateValue);
-                        }
+                        setDatePickerDate(year, month, dayOfMonth, datePickerEditText);
                     }
                 }, actualYear, actualMonth, actualDay);
 
                 datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
-                if (datePickerEditText == beginDate && !endDateValue.equals("")){
-                    try {
-                        datePickerDialog.getDatePicker().setMaxDate((long)Utils.convertDateIntoMillis(endDateValue) + 40000000);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                        Toast.makeText(getApplicationContext(), "The date can't be parsed, please contact support", Toast.LENGTH_LONG).show();
-                    }
-                }
-                if (datePickerEditText == endDate && !beginDateValue.equals("")){
-                    try {
-                        datePickerDialog.getDatePicker().setMinDate((long)Utils.convertDateIntoMillis(beginDateValue) + 40000000);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                        Toast.makeText(getApplicationContext(), "The date can't be parsed, please contact support", Toast.LENGTH_LONG).show();
-                    }
-                }
+                setDatePickerBoundaries(datePickerDialog, datePickerEditText);
                 datePickerDialog.show();
             }
         });
+    }
+
+    // Logic for avoid the begin date and the end date to pass each other
+    private void setDatePickerBoundaries(DatePickerDialog datePickerDialog, EditText datePickerEditText) {
+            try {
+                if (datePickerEditText == beginDate && !endDateValue.equals(""))
+                    datePickerDialog.getDatePicker().setMaxDate((long) Utils.convertDateIntoMillis(endDateValue) + 40000000);
+                else if (datePickerEditText == endDate && !beginDateValue.equals(""))
+                    datePickerDialog.getDatePicker().setMinDate((long) Utils.convertDateIntoMillis(beginDateValue) + 40000000);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "The date can't be parsed, please contact support", Toast.LENGTH_LONG).show();
+            }
+
+    }
+
+    // Display the selected date
+    private void setDatePickerDate(int year, int month, int dayOfMonth, EditText datePickerEditText) {
+        if (datePickerEditText == beginDate){
+            beginDateValue = Utils.addZeroToDate(dayOfMonth)+"/"+ Utils.addZeroToDate(month+1)+"/"+year;
+            datePickerEditText.setText(beginDateValue);
+        }else {
+            endDateValue = Utils.addZeroToDate(dayOfMonth)+"/"+ Utils.addZeroToDate(month+1)+"/"+year;
+            datePickerEditText.setText(endDateValue);
+        }
     }
 }
